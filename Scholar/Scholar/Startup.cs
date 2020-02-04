4bf1d242f02c238fc00ff29e23edce25d3b6b8fb
Eliezer <eliezerbwana21@gmail.com>
2020-02-04 14:35:50 +0200

    configuration du default routing

--- Scholar/Scholar/Startup.cs
+++ Scholar/Scholar/Startup.cs
@@ -18,7 +18,9 @@ namespace Scholar
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+            
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,8 +30,8 @@ namespace Scholar
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            //app.UseRouting();
+            
+            app.UseRouting();
             app.UseMvcWithDefaultRoute();
 
             app.UseEndpoints(endpoints =>
